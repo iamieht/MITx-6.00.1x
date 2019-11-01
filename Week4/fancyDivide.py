3bbd8f4159dc772d31ca4b5fda0adce7499ec87d
Iván E. Hernández T <iamieht@gmail.com>
2019-11-01 18:53:43 +0100

    Week 4
    Week 4

---
+++ Week4/fancyDivide.py
@@ -0,0 +1,15 @@
+def fancy_divide(numbers,index):
+    try:
+        denom = numbers[index]
+        for i in range(len(numbers)):
+            numbers[i] /= denom
+    except IndexError:
+        print("-1")
+    else:
+        print("1")
+    finally:
+        print("0")
+
+#fancy_divide([0, 2, 4], 1)
+#fancy_divide([0, 2, 4], 4)
+fancy_divide([0, 2, 4], 0)
